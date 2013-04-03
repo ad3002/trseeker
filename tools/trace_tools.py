a81253d0ba30d269e4d654a2a9a8d02f00e7f306
Aleksey Komissarov <ad3002@gmail.com>
2013-04-03 22:08:41 +0400

    Read clip dictionary from clip_file_name gz archive

--- tools/trace_tools.py
+++ tools/trace_tools.py
@@ -11,25 +11,33 @@ Functions related to Trace data.
 import os
 from trseeker.seqio.tab_file import sc_iter_simple_tab_file
 from trseeker.seqio.fasta_file import sc_iter_fasta
+import gzip
+
+def get_clip_data(clip_file_name):
+    ''' Read clip dictionary from clip_file_name gz archive.
+    '''
+    id2clip = {}
+    fh = gzip.open(clip_file_name, 'rb')
+    file_content = fh.readlines()
+    for line in file_content:
+        if not line:
+            continue
+        if line.startswith("TI"):
+            continue
+        sid, start, end = map(int, line.strip().split())
+        id2clip[int(sid)] = (start, end)
+    fh.close()
+    return id2clip
 
 def unclip_trace_file(fasta_file, clip_file, uncliped_file):
     ''' Unclip. Remove vector flanks from Trace data.'''
-
-    id2clip = {}
-
-    if os.path.isfile(clip_file):
-        for (id, start, end) in sc_iter_simple_tab_file(clip_file):
-            if id == "TI":
-                continue
-            id2clip[int(id)] = (int(start), int(end))
-
+    id2clip = get_clip_data(clip_file)
     result = []
     for seq_obj in sc_iter_fasta(fasta_file):
         id = int(seq_obj.seq_gi)
         seq = seq_obj.sequence
         if id in id2clip:
-            seq = seq_obj.sequence[id2clip[id][0]: id2clip[id][1]]
-        result.append(">%s\n%s\n" % (id, seq))
-
+            seq_obj.seq_sequence = seq_obj.sequence[id2clip[id][0]: id2clip[id][1]]
+        result.append(seq_obj.fasta)
     with open(uncliped_file, "w") as fh:
         fh.writelines(result)
