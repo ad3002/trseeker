ae454af9038b4c019713a534011529c29308d5f7
Aleksey Komissarov <ad3002@gmail.com>
2013-05-30 22:52:17 +0400

    import fix

--- models/dataset_model.py
+++ models/dataset_model.py
@@ -5,7 +5,7 @@
 #@author: Aleksey Komissarov
 #@contact: ad3002@gmail.com 
 
-from PyExp.models.abstract_model import AbstractModel
+from PyExp import AbstractModel
 
 class DatasetModel(AbstractModel):
     """ Class for dataset wrapping.
