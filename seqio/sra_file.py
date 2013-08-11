6de91e766e8de6e17227cb52a99450de46db7768
Aleksey Komissarov <ad3002@gmail.com>
2013-08-11 16:51:27 +0400

    formating

--- seqio/sra_file.py
+++ seqio/sra_file.py
@@ -11,42 +11,42 @@ TODO: implement this.
 
 class FastqObj(object):
 
-	def __init__(self, head, seq, strain, qual):
-		self.head = head
-		self.seq = seq.lower()
-		self.qual = qual
-		self.strain = strain
-		self.id = head.strip().split()[0].replace("@","")
+    def __init__(self, head, seq, strain, qual):
+        self.head = head
+        self.seq = seq.lower()
+        self.qual = qual
+        self.strain = strain
+        self.id = head.strip().split()[0].replace("@","")
 
-	@property
-	def fastq(self):
-		return "%s%s%s%s" % (
-					self.head,
-					self.seq,
-					self.strain,
-					self.qual,
-			)
+    @property
+    def fastq(self):
+        return "%s%s%s%s" % (
+                    self.head,
+                    self.seq,
+                    self.strain,
+                    self.qual,
+            )
 
-	@property
-	def sequence(self):
-		return self.seq.strip().lower()
+    @property
+    def sequence(self):
+        return self.seq.strip().lower()
 
-	@property
-	def fasta(self):
-		return ">%s\n%s\n" % (
-					self.head.strip(),
-					self.seq.strip(),
-					)
+    @property
+    def fasta(self):
+        return ">%s\n%s\n" % (
+                    self.head.strip(),
+                    self.seq.strip(),
+                    )
 
 def fastq_reader(fastq_file):
-	with open(fastq_file) as fh:
-		while True:
-			try:
-				head = fh.readline()
-				seq = fh.readline()
-				strain = fh.readline()
-				qual = fh.readline()
-				fastq_obj = FastqObj(head, seq, strain, qual)
-				yield fastq_obj
-			except:
-				break
+    with open(fastq_file) as fh:
+        while True:
+            try:
+                head = fh.readline()
+                seq = fh.readline()
+                strain = fh.readline()
+                qual = fh.readline()
+                fastq_obj = FastqObj(head, seq, strain, qual)
+                yield fastq_obj
+            except:
+                break
