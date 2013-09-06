1acdc851876f44bcacec07afb371a37671143f15
Aleksey Komissarov <ad3002@gmail.com>
2013-09-06 23:09:02 +0400

    RepeatMaske

--- models/genome_model.py
+++ models/genome_model.py
@@ -60,3 +60,60 @@ class GenomeModel(AbstractModel):
             self.genome_trs_3000_proc = self.genome_trs_3000_length / float(self.genome_length)
         if not self.genome_mean_gc:
             self.genome_mean_gc = self.genome_sum_gc / self.genome_contigs
+
+class RepeatMaskerAnnotation(AbstractModel):
+    ''' Container for RepeatMasker track.
+    For example from http://hgdownload.cse.ucsc.edu/goldenPath/felCat5/database/rmsk.txt.gz
+
+    Table fields:
+      `bin` smallint(5) unsigned NOT NULL,
+      `swScore` int(10) unsigned NOT NULL,
+      `milliDiv` int(10) unsigned NOT NULL,
+      `milliDel` int(10) unsigned NOT NULL,
+      `milliIns` int(10) unsigned NOT NULL,
+      `genoName` varchar(255) NOT NULL,
+      `genoStart` int(10) unsigned NOT NULL,
+      `genoEnd` int(10) unsigned NOT NULL,
+      `genoLeft` int(11) NOT NULL,
+      `strand` char(1) NOT NULL,
+      `repName` varchar(255) NOT NULL,
+      `repClass` varchar(255) NOT NULL,
+      `repFamily` varchar(255) NOT NULL,
+      `repStart` int(11) NOT NULL,
+      `repEnd` int(11) NOT NULL,
+      `repLeft` int(11) NOT NULL,
+      `id` char(1) NOT NULL,
+    '''
+    
+    dumpable_attributes = [
+        'bin',
+        'swScore',
+        'milliDiv',
+        'milliDel',
+        'milliIns',
+        'genoName',
+        'genoStart',
+        'genoEnd',
+        'genoLeft',
+        'strand',
+        'repName',
+        'repClass',
+        'repFamily',
+        'repStart',
+        'repEnd',
+        'repLeft',
+        'id',
+    ]
+    int_attributes = [
+        'bin',
+        'swScore',
+        'milliDiv',
+        'milliDel',
+        'milliIns',
+        'genoStart',
+        'genoEnd',
+        'genoLeft',
+        'repStart',
+        'repEnd',
+        'repLeft',
+    ]
