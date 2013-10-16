6e237d5166ff3ba0264b2a7726b268db6193f17d
Aleksey Komissarov <ad3002@gmail.com>
2013-10-16 20:21:01 +0400

    SAMFeatureDict

--- seqio/sam_file.py
+++ seqio/sam_file.py
@@ -15,7 +15,33 @@ import re, os
 from trseeker.models.sam_model import SAMModel
 from trseeker.seqio.tab_file import TabDelimitedFileIO
 import csv
+import collections
 
+class SAMFeatureDict(collections.MutableMapping):
+    """A dictionary for SAM features
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.store = dict()
+        self.update(dict(*args, **kwargs))  # use the free update to set keys
+
+    def __getitem__(self, key):
+        return self.store[self.__keytransform__(key)]
+
+    def __setitem__(self, key, value):
+        self.store[self.__keytransform__(key)] = value
+
+    def __delitem__(self, key):
+        del self.store[self.__keytransform__(key)]
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        return len(self.store)
+
+    def __keytransform__(self, key):
+        return key
 
 class SAMFileIO(TabDelimitedFileIO):
     """ 
