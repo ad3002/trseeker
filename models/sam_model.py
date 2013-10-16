c5721720753e9b35d9a33a2939ad9fd5514e3e61
Aleksey Komissarov <ad3002@gmail.com>
2013-10-16 18:40:40 +0400

    SAMModel

---
+++ models/sam_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 28.11.2010
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com 
+
+from PyExp import AbstractModel
+
+class SAMModel(AbstractModel):
+    """ Class for SAM data wrapping.
+
+    Dumpable attributes:
+
+    - QNAME
+    - FLAG
+    - RNAME
+    - POS
+    - MAPQ
+    - CIGAR
+    - RNEXT
+    - PNEXT
+    - TLEN
+    - SEQ
+    - QUAL
+    - features
+    
+    """
+
+    dumpable_attributes = [
+        "QNAME",
+        "FLAG",
+        "RNAME",
+        "POS",
+        "MAPQ",
+        "CIGAR",
+        "RNEXT",
+        "PNEXT",
+        "TLEN",
+        "SEQ",
+        "QUAL",
+        "features",
+    ]
+    
+    int_attributes = [
+        "FLAG",
+        "POS",
+        "MAPQ",
+        "PNEXT",
+        "TLEN",
+    ]
