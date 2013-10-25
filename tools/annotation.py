f9dd17f68d60bfb73c14c8e43d93cef1ae2bb6c3
Aleksey Komissarov <ad3002@gmail.com>
2013-10-25 22:07:01 +0400

    Fix docs and var names

--- tools/annotation.py
+++ tools/annotation.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2007-2013 Aleksey Komissarov ( ad3002@gmail.com )
-# All rights reserved.
-#
-# This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution.
+#@created: 07.09.2010
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+
 
 def compute_intersection_intervals(data_a, data_b):
-    ''' Compute intersection between two datasets.
-    Dataset format:
-        list of tuples (chrName, startPos, endPos, feature_id)
-    '''
+    """ Compute intersection between two datasets.
+    Intersection types: match, nested, overlap, and adjacent
+    @param data_a: list of tuples (chrName, startPos, endPos, feature_id)
+    @param data_b: list of tuples (chrName, startPos, endPos, feature_id)
+    @return: (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, intersection_type) tuple
+    """
     data_a.sort()
     data_b.sort()
-    current_pos = 0
     if not data_a or not data_b:
         return []
     if data_a[0][0] > data_b[0][0]:
@@ -31,7 +31,7 @@ def compute_intersection_intervals(data_a, data_b):
         # -----
         # -----
         if a_start == b_start and a_end == b_end:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "match" ))
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "match"))
             if not data_a or not data_b:
                 break
             (b_chr, b_start, b_end, b_feature) = data_b.pop(0)
@@ -40,7 +40,7 @@ def compute_intersection_intervals(data_a, data_b):
         # -----
         #  ---
         if a_start <= b_start and a_end >= b_end:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "nested" ))
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "nested"))
             if not data_b:
                 break
             (b_chr, b_start, b_end, b_feature) = data_b.pop(0)
@@ -48,15 +48,15 @@ def compute_intersection_intervals(data_a, data_b):
         #  ---
         # -----
         if a_start >= b_start and a_end <= b_end:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "nested" ))
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "nested"))
             if not data_a:
                 break
             (a_chr, a_start, a_end, a_feature) = data_a.pop(0)
             continue
         #     -----
         # -----
-        if a_start > b_start and a_end > b_end  and b_end > a_start:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "overlap" ))
+        if a_start > b_start and a_end > b_end and b_end > a_start:
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "overlap"))
             if not data_b:
                 break
             (b_chr, b_start, b_end, b_feature) = data_b.pop(0)
@@ -64,7 +64,7 @@ def compute_intersection_intervals(data_a, data_b):
         # -----
         #     -----
         if a_start < b_start and a_end < b_end and a_end > b_start:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "overlap" ))
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "overlap"))
             if not data_a:
                 break
             (a_chr, a_start, a_end, a_feature) = data_a.pop(0)
@@ -72,7 +72,7 @@ def compute_intersection_intervals(data_a, data_b):
         # -----
         #         -----
         if a_end == b_start:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "adjacent" ))
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "adjacent"))
             if not data_a:
                 break
             (a_chr, a_start, a_end, a_feature) = data_a.pop(0)
@@ -80,7 +80,7 @@ def compute_intersection_intervals(data_a, data_b):
         #         -----
         # -----
         if b_end == a_start:
-            hits.append( (a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "adjacent" ))
+            hits.append((a_chr, a_start, a_end, a_feature, b_chr, b_start, b_end, b_feature, "adjacent"))
             if not data_b:
                 break
             (b_chr, b_start, b_end, b_feature) = data_b.pop(0)
@@ -102,7 +102,11 @@ def compute_intersection_intervals(data_a, data_b):
     return hits
 
 
-def  test_compute_intersection_intervals():
+def test_compute_intersection_intervals():
+    """ Test for compute_intersection_intervals function.
+    @todo: move it to testing framework
+    @return: None
+    """
     data_a = [
     ("chrA", 0, 10, 0),
     ("chrA", 25, 40, 1),
