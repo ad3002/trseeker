01c2c46096c0f9f9d02052a36233d3a893e68d13
Aleksey Komissarov <ad3002@gmail.com>
2013-10-25 21:54:23 +0400

    Fix docs and var names

--- tools/assembly_tools.py
+++ tools/assembly_tools.py
@@ -4,16 +4,19 @@
 #@created: 07.09.2010
 #@author: Aleksey Komissarov
 #@contact: ad3002@gmail.com 
-'''
+"""
 Functions related to assembler statistics
-'''
+"""
 
-def get_N50(lengths):
-    '''
-    Get (N50 contig length, N50, shortest contig, longest contig) statistics for list of contigs lengths
-    '''
+
+def get_n50(lengths):
+    """
+    Get (N50 contig length, N50, shortest contig, longest contig) statistics for list of contigs lengths.
+    @param lengths: a list of contig lengths
+    @return (N50, L50, shortest contig, longest contig)
+    """
     if not lengths:
-        return (0, 0, 0, 0)
+        return 0, 0, 0, 0
     lengths.sort(reverse=True)
     total = sum(lengths)
     n50 = 0
@@ -24,4 +27,4 @@ def get_N50(lengths):
         l50 += 1
         n50 += x
         if n50 >= total/2:
-            return (x, l50, shortest_seq, longest_seq)
+            return x, l50, shortest_seq, longest_seq
