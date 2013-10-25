7fa21eee45c73b7d3c64fff96a0a1b79c4f65e83
Aleksey Komissarov <ad3002@gmail.com>
2013-10-25 21:46:25 +0400

    Fix docs and var names

--- settings.py
+++ settings.py
@@ -3,38 +3,44 @@
 #
 #@created: 06.09.2011
 #@author: Aleksey Komissarov
-#@contact: ad3002@gmail.com 
-'''
+#@contact: ad3002@gmail.com
+"""
 Trseeker settings loader.
-'''
-
-import yaml, os
+"""
+import os
+import yaml
 import platform 
 
 SETTINGS_FILENAME = "settings.yaml"
 NGRAM_LENGTH = 23
 NGRAM_N = 100000000
-    
+
+
 def load_settings():
-    ''' Load settings from yaml file.'''
-    file = os.path.abspath(__file__)
-    settings_file = os.path.join(os.path.split(file)[0],
+    """ Load settings from yaml file.
+    @return settings
+    """
+    file_name = os.path.abspath(__file__)
+    settings_file = os.path.join(os.path.split(file_name)[0],
                                  SETTINGS_FILENAME)
     with open(settings_file) as fh:
         settings = yaml.load(fh)
     myos = platform.system()
     if myos == "Windows":
-        settings["trseeker"]["os"]  = "WIN"
+        settings["trseeker"]["os"] = "WIN"
     elif myos == "Darwin":
-        settings["trseeker"]["os"]  = "OSX"
+        settings["trseeker"]["os"] = "OSX"
     else: 
-        settings["trseeker"]["os"]  = "NIX"
+        settings["trseeker"]["os"] = "NIX"
     return settings
 
+
 def save_settings(settings):
-    ''' Save settings to yaml file.'''
-    file = os.path.abspath(__file__)
-    settings_file = os.path.join(os.path.split(file)[0],
+    """ Save settings to yaml file.
+    @param settings: trseeker settings
+    """
+    file_name = os.path.abspath(__file__)
+    settings_file = os.path.join(os.path.split(file_name)[0],
                                  SETTINGS_FILENAME)
     with open(settings_file, "w") as fh:
         yaml.dump(fh, settings)
