dee894ad93594bb0e70d9cd9b8bf3f63e82846c0
Aleksey Komissarov <ad3002@gmail.com>
2022-02-08 16:33:48 +0300

    feat/add download_items_from_ncbi

--- tools/entrez_database.py
+++ tools/entrez_database.py
@@ -49,5 +49,46 @@ def download_proteins_from_ncbi(query, output_file, email, batch=500, verbose_st
     return proteins
 
 
+def download_items_from_ncbi(query, 
+                             database, 
+                             output_file, 
+                             email, 
+                             rettype="tasta", 
+                             retmode="text", 
+                             batch=500, 
+                             verbose_step=1000):
+    '''
+    '''
+    Entrez.email = email
+    
+    start = 0
+    proteins = []
+    while True:
+        handle = Entrez.esearch(db=database, retmax=batch, retstart=start, term=query)
+        record = Entrez.read(handle)
+        handle.close()
+        start += batch
+        if start % verbose_step == 0:
+            print(len(proteins), end=" ")
+        proteins += record["IdList"]
+        if len(record["IdList"]) == 0:
+            break
+    print()
+    print(f"Downloaded {len(proteins)} proteins")
+
+    with open(output_file, "w") as fw:
+        for i in range(0,len(proteins), batch):
+            if i % verbose_step == 0:
+                print(len(i), end=" ")
+            handle = Entrez.efetch(db=database, 
+                                   id=proteins[i:i+batch], 
+                                   rettype=rettype, 
+                                   retmode=retmode)
+            record = handle.read().replace("\n\n","\n")
+            fw.write(record.strip())
+            fw.write("\n")
+    return proteins
+
+
 
 
